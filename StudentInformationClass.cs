3189b948ae5ddb84cbd5e5201652c74552bf9531
L1PC36 <L1PC36@DESKTOP-4JGCP7N>
2024-10-03 10:48:13 +0800

    Add project files.

---
+++ StudentInformationClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizationProfile
+{
+    class StudentInformationClass
+    {
+        public static int SetStudentNo = 0, SetAge = 0;
+        public static string SetProgram = " ", SetContactNo = " ", SetGender = " ", SetBirthday = " ", SetFullName = " ";
+    }
+}
